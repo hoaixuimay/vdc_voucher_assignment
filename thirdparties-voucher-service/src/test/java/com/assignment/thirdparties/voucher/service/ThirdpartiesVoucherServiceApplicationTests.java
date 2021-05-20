8b9876d520e17d496ae0317aafbea47b3e58a1c3
hoaipham <hoaixuimay@gmail.com>
2021-05-20 14:08:16 +0700

    Initial

---
+++ thirdparties-voucher-service/src/test/java/com/assignment/thirdparties/voucher/service/ThirdpartiesVoucherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.thirdparties.voucher.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdpartiesVoucherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ voucher-service/src/test/java/com/assignment/voucher/service/VoucherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.voucher.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
