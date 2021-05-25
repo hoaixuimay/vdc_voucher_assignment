9b48f0e186a8d1ab989ad6a7236749a7975d65df
[Vision] Hoai Pham <hoai.pham@axonactive.com>
2021-05-25 14:46:08 +0700

    update coverage test

--- voucher-service/src/test/java/com/assignment/voucher/service/repository/VoucherRepositoryTest.java
+++ voucher-service/src/test/java/com/assignment/voucher/service/repository/VoucherRepositoryTest.java
@@ -48,16 +48,19 @@ public class VoucherRepositoryTest {
     @Test
     public void findByPhoneNumber_whenHasData_thenReturnCorrectly() {
         Voucher voucher = new Voucher();
+        //voucher.setId(1);
         voucher.setPhoneNumber(MOCK_PHONE_NUMBER);
         voucher.setVoucherCode(MOCK_VOUCHER_NUMBER);
         testEntityManager.persist(voucher);
 
         Voucher voucher2 = new Voucher();
+        //voucher2.setId(2);
         voucher2.setPhoneNumber(MOCK_PHONE_NUMBER);
         voucher2.setVoucherCode(MOCK_VOUCHER_NUMBER_TWO);
         testEntityManager.persist(voucher2);
 
         Voucher voucher3 = new Voucher();
+        //voucher3.setId(3);
         voucher3.setPhoneNumber(MOCK_PHONE_NUMBER_TWO);
         voucher3.setVoucherCode(MOCK_VOUCHER_NUMBER_THREE);
         testEntityManager.persist(voucher3);
@@ -65,10 +68,16 @@ public class VoucherRepositoryTest {
         List<Voucher> vouchers = voucherRepository.findByPhoneNumber(MOCK_PHONE_NUMBER);
         Assertions.assertEquals(2, vouchers.size());
         assertThat(vouchers.get(0).getVoucherCode(), anyOf(is(MOCK_VOUCHER_NUMBER), is(MOCK_VOUCHER_NUMBER_TWO)));
+        assertThat(vouchers.get(0).getId(), anyOf(is(1L), is(2L)));
+        assertThat(vouchers.get(0).getPhoneNumber(), is(MOCK_PHONE_NUMBER));
         assertThat(vouchers.get(1).getVoucherCode(), anyOf(is(MOCK_VOUCHER_NUMBER), is(MOCK_VOUCHER_NUMBER_TWO)));
+        assertThat(vouchers.get(1).getId(), anyOf(is(1L), is(2L)));
+        assertThat(vouchers.get(1).getPhoneNumber(), is(MOCK_PHONE_NUMBER));
 
         vouchers = voucherRepository.findByPhoneNumber(MOCK_PHONE_NUMBER_TWO);
         Assertions.assertEquals(1, vouchers.size());
         assertThat(vouchers.get(0).getVoucherCode(), is(MOCK_VOUCHER_NUMBER_THREE));
+        assertThat(vouchers.get(0).getId(), is(3L));
+        assertThat(vouchers.get(0).getPhoneNumber(), is(MOCK_PHONE_NUMBER_TWO));
     }
 }
--- voucher-service/src/test/java/com/assignment/voucher/service/service/VoucherServiceTest.java
+++ voucher-service/src/test/java/com/assignment/voucher/service/service/VoucherServiceTest.java
@@ -1,6 +1,8 @@
 package com.assignment.voucher.service.service;
 
 import com.assignment.voucher.service.dto.VoucherDto;
+import com.assignment.voucher.service.dto.VoucherRequest;
+import com.assignment.voucher.service.dto.VoucherWaitResponse;
 import com.assignment.voucher.service.model.Voucher;
 import com.assignment.voucher.service.repository.VoucherRepository;
 
@@ -11,11 +13,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @ExtendWith(MockitoExtension.class)
 public class VoucherServiceTest {
@@ -27,6 +38,10 @@ public class VoucherServiceTest {
     @Mock private VoucherRepository voucherRepository;
     @Mock private RestTemplateBuilder restTemplateBuilder;
     @Mock private Environment env;
+    @Mock private RestTemplate restTemplate;
+
+    @Autowired
+    private MockRestServiceServer mockRestServiceServer;
 
     @InjectMocks
     private VoucherService voucherService;
@@ -45,6 +60,29 @@ public class VoucherServiceTest {
         Assertions.assertEquals(2, result.size());
         Assertions.assertEquals(MOCK_VOUCHER_CODE, result.get(0).getVoucherCode());
         Assertions.assertEquals(MOCK_VOUCHER_CODE_TWO, result.get(1).getVoucherCode());
+    }
+
+//    @Test
+//    public void buyVoucher_whenRunLong_thenReturnMessage() throws ExecutionException, InterruptedException {
+//        // Mock timeout to 1
+//
+//        Mockito.when(env.getProperty("requestTimeoutInSeconds")).thenReturn("1");
+//        Mockito.when(restTemplateBuilder.build()).thenReturn(restTemplate);
+//        Mockito.when(restTemplate.getForObject(Mockito.anyString(), Mockito.any(Class.class), Mockito.any(Map.class))).thenReturn(new VoucherDto());
+//        this.mockRestServiceServer
+//                .expect(MockRestRequestMatchers.requestTo("/api/3rd/voucher"))
+//                .andRespond(MockRestResponseCreators.withSuccess("{}", MediaType.APPLICATION_JSON));
+//
+//        VoucherRequest voucherRequest = new VoucherRequest();
+//        voucherRequest.setDelayInSeconds(2);
+//        voucherRequest.setPhoneNumber(MOCK_PHONE_NUMBER);
+//        Object result = voucherService.buyVoucher(voucherRequest);
+//        Assertions.assertTrue(result instanceof VoucherWaitResponse);
+//    }
+
+    @Test
+    public void buyVoucher_whenRunQuick_thenReturnVoucherCode() {
+
     }
 
     private List<Voucher> mockListVoucherReturn() {
